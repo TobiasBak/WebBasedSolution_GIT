01e483ce29d5a6b32b2348199f21c63d5b19305d
Kevin Torp <Kevintorp@hotmail.com>
2024-10-22 15:07:23 +0200

    added Andreas' frontend changes

---
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/whyline.js
@@ -0,0 +1,18 @@
+const jsonDataFile = "./whyLineData.json";
+
+function fetchJSONData() {
+    fetch(jsonDataFile)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) =>
+            console.log(data))
+        .catch((error) =>
+            console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
+console.log(window.location.pathname);
