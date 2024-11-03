a3f4ec93f80683cf52edd9eaa819ec09d0e51fa3
Kevin Torp <Kevintorp@hotmail.com>
2024-11-03 11:49:02 +0100

    Changed to reading the right log file

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/scenario_log_data_converter.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/scenario_log_data_converter.js
@@ -1,7 +1,7 @@
 // We are going to fetch localhost with a mode to allow CORS.
 // We are simply fetching scenario_log.json which is in WebRoot.
 // Fetching the JSON file.
-const dataPath = './scenario_log.json';
+const dataPath = './logs/scenario_log.json';
 function loadData(path) {
     fetch(path, {
         mode: 'no-cors'
