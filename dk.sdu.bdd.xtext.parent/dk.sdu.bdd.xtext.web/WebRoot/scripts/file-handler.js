c6c19564d0ce3ce6e6e50194278eafde2e356a4e
TobiasBak <tobib21@student.sdu.dk>
2024-11-08 09:29:12 +0100

    Cachebuster!

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/file-handler.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/file-handler.js
@@ -1,11 +1,12 @@
 function loadData(path) {
     const request = new XMLHttpRequest();
-    request.open('GET', path, false);
+    const cacheBuster = `?t=${new Date().getTime()}`;
+    request.open('GET', path + cacheBuster, false);
     request.send(null);
 
     if (request.status === 200) {
         return JSON.parse(request.responseText);
     } else {
-        throw new Error("Failed to load data from ${path}");
+        throw new Error(`Failed to load data from ${path}`);
     }
 }
